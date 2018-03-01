87302609828eba95a7fafc14587ebe4045a4db01
Kaitlyn Morrell <kjm538@nyu.edu>
2018-03-01 18:22:06 -0500

    lab manual speaks the truth (sometimes)

--- nmr/plot_ispin_nmr_data_FFT.py
+++ nmr/plot_ispin_nmr_data_FFT.py
@@ -1,10 +1,10 @@
 '''
 file:  plot_ispin_nmr_data_FFT.py
-This routine reads in a spincore ".txt" file, and plots the time data 
+This routine reads in a spincore ".txt" file, and plots the time data
 in that file as well as the frequency data (FFT).  Since the interesting
 part of the data is at low frequencies, the frequency range of the plot is
-reduced by a scale factor 'sf'. 
-  
+reduced by a scale factor 'sf'.
+
 Last update:  9/25/2012, 10/07/2012 by Tycho Sleator
 '''
 
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 
 # The following is the filename containing the data
-fname = "T1_sweep00.txt"
+fname = "data/fid/fid0.txt"
 
 # The factor by which the frequency scale is expanded around f=0
 sf = 1.  # PK CHANGE: Converted sf from int to float
@@ -34,15 +34,15 @@ print 'bw = ',bw
 print '1/bw = ',1/bw  # Note that the time interval between points is 1/bw
 
 # Read the data from the the file starting on line 13
-s1 = mlab.csv2rec(fname, skiprows=12)  
+s1 = mlab.csv2rec(fname, skiprows=12)
 
 t =  (1/bw)*np.arange(len(s1)/2)  # time data
 npts = len(t)  # number of data points
 
 # assign the data to variables with shorter names
 s = s1['data']
-rs = s.reshape(-1,2) 
-rtp= np.transpose(rs) # rtp[0] is the real part and 
+rs = s.reshape(-1,2)
+rtp= np.transpose(rs) # rtp[0] is the real part and
                       # rtp[1] is the imaginary part of the data
 
 za = rtp[0] + rtp[1]*1j # create complex array
@@ -51,7 +51,7 @@ za = rtp[0] + rtp[1]*1j # create complex array
 # maxza = np.max([max(za.real),max(za.imag)])  # maximum value
 
 # create the figure
-fig1   = plt.figure(1, figsize = (8,10) )               
+fig1   = plt.figure(1, figsize = (8,10) )
 ax1    = fig1.add_subplot(211)  # this will show that time data
 ax2    = fig1.add_subplot(212)  # this will show the frequency data
 '''
@@ -96,7 +96,7 @@ ax2.axvline(color ='k')
 ax2.plot(f*fscale,fza.real, '-b')  # plot the real part (blue)
 ax2.plot(f*fscale,fza.imag, '-r')  # plot the imaginary part (red)
 # plot the magnitude (black)
-ax2.plot(f*fscale,np.sqrt(fza.real**2 + fza.imag**2), '-k')  
+ax2.plot(f*fscale,np.sqrt(fza.real**2 + fza.imag**2), '-k')
 
 # label the axes
 ax2.set_xlabel('Frequency ('+np.str(funits)+')',fontsize=14)
