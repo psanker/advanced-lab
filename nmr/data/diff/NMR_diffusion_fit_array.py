7c56ab9f63ee952ee50bf8e646f78c406b22e03c
Kaitlyn Morrell <kjm538@nyu.edu>
2018-03-07 06:56:12 -0500

    tweaked diffusion fit

--- nmr/data/diff/NMR_diffusion_fit_array.py
+++ nmr/data/diff/NMR_diffusion_fit_array.py
@@ -36,6 +36,7 @@ print("-------------------------------------------------------------------------
 
 t = 0.001 * np.array([20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210])
 s =  np.array([1.0, 0.94718535466131604, 0.87500470082775494, 0.78395929858123392, 0.66399799540827709, 0.53876116636950855, 0.41159572116718196, 0.29583746678893802, 0.20333697170540407, 0.12903062779845367, 0.078455170818481856, 0.045821627617545005, 0.025183048039646092, 0.014545226164819645, 0.0081264606110035813, 0.0061267654812352376, 0.0059990251642263329, 0.0048322601995357062, 0.0048984050766543519, 0.0052666298354899378])
+gamma = 5.59 #gyromagnetic ratio
 print("t = ", t)
 print("s = ", s)
 
@@ -62,7 +63,7 @@ nlfit,nlpcov = scipy.optimize.curve_fit(f, xdata, ydata, p0=[a0, b0, ...],
 '''
 # Define nonlinear fitting function.
 def f(t, A, alpha, y0):
-    return A*np.exp(-(alpha*(t**3))) + y0
+    return A*np.exp(-((gamma**2)*alpha*(t**3))/12) + y0
 
 # Initial guesses: modify these if fit doesn't converge.
 A0 = 1.0
@@ -77,6 +78,9 @@ nlfit, nlpcov = scipy.optimize.curve_fit(f, t, s, p0=[A0, alpha0, y00], sigma=No
 A_nlfit = nlfit[0]
 alpha_nlfit = nlfit[1]
 y0_nlfit = nlfit[2]
+print('A = {}'.format( A_nlfit))
+print('alpha = {}'.format( alpha_nlfit))
+print('y0 = {}'.format( y0_nlfit))
 
 '''
 Below are the uncertainties in the estimates:  (note that "nlpcov" is the
@@ -129,7 +133,7 @@ plt.legend(loc='upper right' )
 # Plot some text with model equation and values of fit parameters.
 # Use Latex equation style - $ ... $:
 plt.text(0.010, 0.4,
-    '$y \, =\, A e^{-\\alpha t^3} + y_0$',
+    '$y \, =\, A e^{-(\\gamma^2\\alpha t^3)/12} + y_0$',
     ha='left', va='bottom', size='large')
 plt.text(0.010, 0.3,
     '$\\alpha \, =\, ({0:6.4g} \pm {1:5.2g})/s^3$'.format(alpha_nlfit, alphasig_nlfit),
